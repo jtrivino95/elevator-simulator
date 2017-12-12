203909b0a6fcd1c7b038704b607933ecfe3f9044
Javier Triviño Monedero <JTM648@eps.uib.es>
2017-12-12 18:30:28 +0100

    Initial commit

---
+++ main.c
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.c
+ * Author: jtm648
+ *
+ * Created on 12 de diciembre de 2017, 18:26
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <p30F4011.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=elevator-simulator.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/elevator-simulator.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=elevator-simulator.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/elevator-simulator.x.tar
